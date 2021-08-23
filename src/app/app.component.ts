791f850f720406be00d00193a315940835eb71b0
AndhyGT <andhyralac11@gmail.com>
2021-08-22 22:25:20 -0600

    Primera App con Angular y la Api de CoinGecko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+
+interface Coin {
+  id: string;
+  name: string,
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-CoinGecko';
+export class AppComponent implements OnInit{
+
+  coins: Coin[] = [];
+  filteredCoin: Coin[] = [];
+  titles: string[] = [
+    '#',
+    'Coin',
+    'Price',
+    'Price Change',
+    '24h Volume',
+  ];
+
+  searchText: string = '';
+
+  constructor(private http: HttpClient){}
+
+  searchCoin(){
+    this.filteredCoin = this.coins.filter((coin) => 
+      coin.name.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase()) ||
+      coin.symbol.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase())
+    );
+  }
+
+  ngOnInit(){
+    this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .subscribe(
+        res => {
+          this.coins = res;
+          this.filteredCoin = res;
+        },
+        err => console.log(err));
+  }
 }
